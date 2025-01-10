f4d9fb981fa63add12e67d15a07be76ee9d8e956
JanBaig <janatbaig2@gmail.com>
2025-01-10 15:02:13 -0500

    Refactor init accessor config

--- lib/Sema/TypeCheckStorage.cpp
+++ lib/Sema/TypeCheckStorage.cpp
@@ -2445,9 +2445,7 @@ static AccessorDecl *createInitAccessorPrototype(AbstractStorageDecl *storage,
 
   // Set as synthesized and configure the body kind
   initAccessor->setSynthesized();
-  initAccessor->configureAsSILSynthesized(
-    AbstractFunctionDecl::BodyKind::SILSynthesize, 
-    AbstractFunctionDecl::SILSynthesizeKind::Init);
+  initAccessor->setIsPropertyWrapperInitAccessor();
   
   return initAccessor;
 } 
