0bd17e54850edbe872303c44521f49ef136c429c
JanBaig <janatbaig2@gmail.com>
2025-01-10 13:59:14 -0500

    WIP: Set BodyKind for Synthesized Init Accessor

--- include/swift/AST/Decl.h
+++ include/swift/AST/Decl.h
@@ -7411,8 +7411,8 @@ public:
   enum class SILSynthesizeKind {
     None,
     MemberwiseInitializer,
-    DistributedActorFactory
-
+    DistributedActorFactory,
+    Init
     // This enum currently needs to fit in a 2-bit bitfield.
   };
 
@@ -8416,6 +8416,11 @@ public:
 
   bool isInitAccessor() const {
     return (getAccessorKind() == AccessorKind::Init);
+  }
+
+  void configureAsSILSynthesized(BodyKind bodyKind, SILSynthesizeKind synthesizeKind) {
+    setBodyKind(bodyKind);
+    setSILSynthesizeKind(synthesizeKind);
   }
 
   /// \returns true if this is non-mutating due to applying a 'mutating'
