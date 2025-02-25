1599306b947308e09476946cd7eb1b60f41d0098
JanBaig <janatbaig2@gmail.com>
2025-02-25 12:54:03 -0500

    Emit `newValue` via the `emitImplicitConstructoArg`

---
+++ test/Sema/jan-test.swift
@@ -0,0 +1,18 @@
+// RUN: %target-swift-emit-silgen -verify %s
+
+@propertyWrapper 
+struct Wrapper {
+    var wrappedValue: [Int]
+    init(wrappedValue: [Int]) { self.wrappedValue = wrappedValue }
+}
+
+struct myStruct {
+    @Wrapper var x: [Int]
+    init(x: [Int]) { self.x = x}
+}
+
+myStruct(x: [4])
+
+
+
+
